d072bc1e915a35beb67b89ad10283f8220cd827c
ryan1230x <ryan1230tenerife@gmail.com>
2022-04-09 11:15:44 +0100

    remove junk

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,16 +2,10 @@
 
 namespace App\Providers;
 
-use App\Services\Contracts\TicketServiceContract;
-use App\Services\TicketService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-
-    private const CLASSES = [
-        TicketServiceContract::class => TicketService::class,
-    ];
 
     /**
      * bind contracts to service classes
@@ -20,9 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        foreach (self::CLASSES as $abstract => $concrete) {
-            $this->app->bind($abstract, $concrete);
-        }
+        //
     }
 
     /**
